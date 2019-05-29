5c7fa3155c75ff9fb6f756540e3d1ef5655cd93b
Arci <Arcidev@users.noreply.github.com>
2019-05-29 18:45:11 +0200

    Added projects for new Client in .Net Core 3.0 to solution

---
+++ Client.UI/ViewModels/User/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Client.UI.ViewModels.User
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
